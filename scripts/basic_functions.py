caee3c38d2128982d4cd77c335f855e7fd8a0803
HeinDuijf <duijf.hein@gmail.com>
2023-08-23 13:48:43 +0200

    added calculate_accuracy_and_precision

--- scripts/basic_functions.py
+++ scripts/basic_functions.py
@@ -1,21 +1,34 @@
 import random as rd
 
+from statsmodels.stats.proportion import proportion_confint
+
 import scripts.config as cfg
 
 
 def majority_winner(values: list):
     """ Basic function to determine the majority winner in a binary decision context."""
-    number_votes_for_elites = len(
-        [value for value in values if value == cfg.vote_for_elites]
-    )
-    number_votes_for_mass = len(values) - number_votes_for_elites
+    options = sorted(set(values))
     threshold = len(values) / 2
-    if number_votes_for_elites > threshold:
-        return cfg.vote_for_elites
-    elif number_votes_for_mass > threshold:
-        return cfg.vote_for_mass
-    else:
-        return rd.choice([cfg.vote_for_mass, cfg.vote_for_elites])
+    for option in options:
+        if values.count(option) > threshold:
+            return option
+    return rd.choice(options)
+
+
+def calculate_accuracy_and_precision(list_of_items, alpha: float = 0.05):
+    number_of_items = len(list_of_items)
+    number_of_success = len(
+        [outcome for outcome in list_of_items if outcome == cfg.vote_for_positive]
+    )
+    estimated_accuracy = number_of_success / number_of_items
+    confidence_interval = proportion_confint(
+        number_of_success, number_of_items, alpha=alpha
+    )
+    result = {
+        "accuracy": estimated_accuracy,
+        "precision": max(confidence_interval) - min(confidence_interval),
+    }
+    return result
 
 
 def convert_math_to_text(string: str):
