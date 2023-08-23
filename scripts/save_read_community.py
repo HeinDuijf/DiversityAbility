37ca335ce57a3178af82f2ae9e4f59dbb750050f
HeinDuijf <duijf.hein@gmail.com>
2023-08-23 13:50:17 +0200

    added sources

--- scripts/save_read_community.py
+++ scripts/save_read_community.py
@@ -6,44 +6,62 @@ from community import Community
 
 def community_compress(community: Community):
     edges_dict = {}
-    for source, target in community.network.edges():
+    for source, target in community.influence_network.edges():
         if source not in edges_dict.keys():
             edges_dict[source] = f"{target}"
         else:
             edges_dict[source] = f"{edges_dict[source]},{target}"
+    source_edges_dict = {}
+    for agent, source in community.source_network.edges():
+        if agent not in source_edges_dict.keys():
+            source_edges_dict[f"S{agent}"] = f"{source}"
+        else:
+            source_edges_dict[f"S{agent}"] = f"{source_edges_dict[agent]},{source}"
     community_dict = {
-        "N": community.number_of_nodes,
+        "N": community.number_of_agents,
         "E": community.number_of_elites,
-        "d": community.degree,
-        "pe": community.elite_competence,
-        "pm": community.mass_competence,
+        "d": community.influence_degree,
+        "NS": community.number_of_sources,
+        "ds": community.source_degree,
         "pp": community.probability_preferential_attachment,
         "h": community.probability_homophilic_attachment,
         **edges_dict,
+        **source_edges_dict,
     }
     return community_dict
 
 
 def community_unpack(community_compressed: dict):
-    nodes = [
-        node
-        for node in range(community_compressed["N"])
-        if node in community_compressed.keys()
+    agents = [
+        agent
+        for agent in range(community_compressed["N"])
+        if agent in community_compressed.keys()
     ]
     edges = [
         (source, int(target))
-        for source in nodes
-        for target in community_compressed[source].split(",")
+        for source in agents
+        for target in agents
+        if str(target) in community_compressed[source].split(",")
+    ]
+    sources = [
+        source for source in [f"s{k}" for k in range(community_compressed["NS"])]
+    ]
+    source_edges = [
+        (agent, source)
+        for agent in agents
+        for source in sources
+        if str(source) in community_compressed[f"S{agent}"].split(",")
     ]
     community_dict = {
-        "number_of_nodes": community_compressed["N"],
+        "number_of_agents": community_compressed["N"],
         "number_of_elites": community_compressed["E"],
-        "degree": community_compressed["d"],
-        "elite_competence": community_compressed["pe"],
-        "mass_competence": community_compressed["pm"],
+        "influence_degree": community_compressed["d"],
+        "number_of_sources": community_compressed["NS"],
+        "source_degree": community_compressed["ds"],
         "probability_preferential_attachment": community_compressed["pp"],
         "probability_homophilic_attachment": community_compressed["h"],
         "edges": edges,
+        "source_edges": source_edges,
     }
     community = Community(**community_dict)
     return community
--- tests/test_save_read_community.py
+++ tests/test_save_read_community.py
@@ -11,48 +11,52 @@ from scripts.save_read_community import (
 
 def test_community_compress():
     params: dict = {
-        "number_of_nodes": 100,
-        "number_of_elites": 35,
-        "degree": 7,
-        "elite_competence": 0.75,
-        "mass_competence": 0.52,
+        "number_of_agents": 3,
+        "number_of_elites": 2,
+        "influence_degree": 2,
+        "number_of_sources": 11,
+        "source_degree": 1,
         "probability_homophilic_attachment": 0.8,
         "probability_preferential_attachment": 0.4,
-        "edges": [(0, 1), (0, 2), (1, 2), (1, 9), (3, 10)],
+        "edges": [(0, 1), (0, 2), (1, 2), (1, 0), (2, 1)],
+        "source_edges": [(0, "s0"), (1, "s1"), (2, "s2")],
     }
     community = Community(**params)
     community_compressed = community_compress(community)
-    assert community_compressed["N"] == params["number_of_nodes"]
+    assert community_compressed["N"] == params["number_of_agents"]
     assert community_compressed["E"] == params["number_of_elites"]
-    assert community_compressed["d"] == params["degree"]
-    assert community_compressed["pe"] == params["elite_competence"]
-    assert community_compressed["pm"] == params["mass_competence"]
+    assert community_compressed["d"] == params["influence_degree"]
     assert community_compressed["h"] == params["probability_homophilic_attachment"]
     assert community_compressed["pp"] == params["probability_preferential_attachment"]
     assert community_compressed[0] == "1,2"
-    assert community_compressed[1] == "2,9"
-    assert community_compressed[3] == "10"
+    assert community_compressed[1] == "2,0"
+    assert community_compressed[2] == "1"
+    assert community_compressed["S0"] == "s0"
+    assert community_compressed["S1"] == "s1"
+    assert community_compressed["S2"] == "s2"
 
 
 def test_community_unpack():
     community_compressed: dict = {
-        "N": 100,
-        "E": 35,
-        "d": 7,
-        "pe": 0.75,
-        "pm": 0.52,
+        "N": 3,
+        "E": 2,
+        "d": 2,
+        "NS": 11,
+        "ds": 1,
         "h": 0.8,
         "pp": 0.4,
+        "e": [(0, 1), (0, 2), (1, 2), (1, 0), (2, 1)],
         0: "1,2",
-        1: "2,9",
-        3: "10,2",
+        1: "2,0",
+        2: "1",
+        "S0": "s0",
+        "S1": "s1",
+        "S2": "s2",
     }
     community_unpacked = community_unpack(community_compressed)
-    assert community_unpacked.number_of_nodes == community_compressed["N"]
+    assert community_unpacked.number_of_agents == community_compressed["N"]
     assert community_unpacked.number_of_elites == community_compressed["E"]
-    # assert community_unpacked.degree == params["d"]
-    assert community_unpacked.elite_competence == community_compressed["pe"]
-    assert community_unpacked.mass_competence == community_compressed["pm"]
+    # assert community_unpacked.influence_degree == params["d"]
     assert (
         community_unpacked.probability_homophilic_attachment
         == community_compressed["h"]
@@ -61,31 +65,36 @@ def test_community_unpack():
         community_unpacked.probability_preferential_attachment
         == community_compressed["pp"]
     )
-    for node in community_unpacked.nodes:
-        if node in community_compressed.keys():
-            assert list(community_unpacked.network[node]) == [
-                int(neighbor) for neighbor in community_compressed[node].split(",")
-            ]
+    for agent in community_unpacked.agents:
+        if agent in community_compressed.keys():
+            assert set(community_unpacked.influence_network[agent]) == set(
+                [int(neighbor) for neighbor in community_compressed[agent].split(",")]
+            )
+    assert community_unpacked.number_of_sources == community_compressed["NS"]
+    assert all(
+        [
+            set(list(community_unpacked.source_network[agent]))
+            == set(community_compressed[f"S{agent}"].split(","))
+            for agent in community_unpacked.agents
+            if f"S{agent}" in community_compressed.keys()
+        ]
+    )
 
 
 def test_save_and_read():
     params: dict = {
-        "number_of_nodes": 100,
+        "number_of_agents": 100,
         "number_of_elites": 35,
-        "degree": 7,
-        "elite_competence": 0.75,
-        "mass_competence": 0.52,
+        "influence_degree": 7,
         "probability_homophilic_attachment": 0.8,
         "probability_preferential_attachment": 0.4,
     }
     community = Community(**params)
     save_community_to_file(filename="data/test_community", community=community)
     community_read = read_community_from_file(filename="data/test_community")
-    assert community.number_of_nodes == community_read.number_of_nodes
+    assert community.number_of_agents == community_read.number_of_agents
     assert community.number_of_elites == community_read.number_of_elites
-    assert community.degree == community_read.degree
-    assert community.elite_competence == community_read.elite_competence
-    assert community.mass_competence == community_read.mass_competence
+    assert community.influence_degree == community_read.influence_degree
     assert (
         community.probability_homophilic_attachment
         == community_read.probability_homophilic_attachment
@@ -94,8 +103,8 @@ def test_save_and_read():
         community.probability_preferential_attachment
         == community_read.probability_preferential_attachment
     )
-    community_edges = list(community.network.edges())
-    community2_edges = list(community_read.network.edges())
+    community_edges = list(community.influence_network.edges())
+    community2_edges = list(community_read.influence_network.edges())
     for edge in community_edges:
         assert edge in community2_edges
     os.remove("data/test_community.pickle")
