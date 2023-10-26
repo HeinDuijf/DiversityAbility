a538ccb64bb276dd7a521772e00138504b2afa18
HeinDuijf <duijf.hein@gmail.com>
2023-10-26 14:40:11 +0200

    updated terminology

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     # Variables for the size of the simulation
     number_of_communities = 10 ** 5
     number_of_voting_simulations = 10 ** 5
-    number_of_nodes = 10 ** 2
+    number_of_agents = 10 ** 2
 
     Simulation(
         filename_csv="data/clean",
         folder_communities="data/communities",
         number_of_communities=number_of_communities,
         number_of_voting_simulations=number_of_voting_simulations,
-        number_of_nodes=number_of_nodes,
+        number_of_agents=number_of_agents,
         elite_competence_range=(0.55, 0.7),
         mass_competence_range=(0.55, 0.7),
         number_of_elites_range=(25, 45),
