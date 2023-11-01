e1a7539426c832b3a713ea3b1dbdd498421ea682
HeinDuijf <duijf.hein@gmail.com>
2023-11-01 21:01:09 +0100

    updated with SimulationTeams

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from simulation import Simulation
+from simulation import SimulationTeams
 
 if __name__ == "__main__":
-    # Variables for the size of the simulation
-    number_of_communities = 10 ** 5
-    number_of_voting_simulations = 10 ** 5
-    number_of_agents = 10 ** 2
-
-    Simulation(
-        filename_csv="data/clean",
-        folder_communities="data/communities",
-        number_of_communities=number_of_communities,
-        number_of_voting_simulations=number_of_voting_simulations,
-        number_of_agents=number_of_agents,
-        elite_competence_range=(0.55, 0.7),
-        mass_competence_range=(0.55, 0.7),
-        number_of_elites_range=(25, 45),
-        probability_homophilic_attachment_range=(0.5, 0.75),
-    ).run()
+    params = {
+        "group_types": ["best", "most_diverse", "random"],
+        "source_reliability_range": (0.5, 0.7),
+        "number_of_agents": 20,
+        "number_of_communities": 10 ** 3,
+        "number_of_voting_simulations": 10 ** 3,
+        "source_degree": 5,
+    }
+    group_sizes = [5, 11]  # 21]
+    sources_range = [11, 21]  # 21, 31]
+    for number_of_sources in sources_range:
+        for group_size in group_sizes:
+            print(
+                f"Started: Number of sources {number_of_sources} | Group size:"
+                f" {group_size}"
+            )
+            SimulationTeams(
+                group_size=group_size, number_of_sources=number_of_sources, **params,
+            ).run()
