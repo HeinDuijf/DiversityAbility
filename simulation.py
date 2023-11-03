a3d9d122f51347628fdf2a089f2c14bd74a9228a
HeinDuijf <duijf.hein@gmail.com>
2023-11-03 15:09:41 +0100

    updated Simulation

--- simulation.py
+++ simulation.py
@@ -1,16 +1,13 @@
 import concurrent.futures as cf
 import copy
 import os
-import random as rd
 import shutil
 import time
 from statistics import mean
 
-import numpy as np
-
 import utils.config as cfg
 from community import Community
-from determine_teams import best_group, most_diverse_group, random_group
+from determine_teams import best_team, most_diverse_team, random_team
 from utils.basic_functions import calculate_accuracy_and_precision
 from utils.save_read_community import save_community_to_file
 
@@ -23,12 +20,12 @@ class Simulation:
         folder_communities: str = None,
         filename_csv: str = None,
         number_of_agents: int = 100,
-        degree: int = 6,
-        probability_preferential_attachment: float = 0.6,
-        elite_competence_range=(0.55, 0.7),
-        mass_competence_range=(0.55, 0.7),
-        number_of_elites_range=(25, 45),
-        probability_homophilic_attachment_range=(0.5, 0.75),
+        influence_degree: int = 6,
+        group_size=10,
+        group_types=("best", "most_diverse", "random"),
+        number_of_sources=10,
+        source_degree=5,
+        source_reliability_range=(0.5, 0.7),
     ):
         self.start_time = time.time()
         time_str = time.strftime("%Y%m%d_%H%M%S")
@@ -39,14 +36,13 @@ class Simulation:
         self.number_of_communities = number_of_communities
         self.number_of_voting_simulations = number_of_voting_simulations
         self.number_of_agents = number_of_agents
-        self.degree = degree
-        self.probability_preferential_attachment = probability_preferential_attachment
-        self.elite_competence_range = elite_competence_range
-        self.mass_competence_range = mass_competence_range
-        self.number_of_elites_range = number_of_elites_range
-        self.probability_homophilic_attachment_range = (
-            probability_homophilic_attachment_range
-        )
+        self.influence_degree = influence_degree
+
+        self.group_size = group_size
+        self.group_types = group_types
+        self.number_of_sources = number_of_sources
+        self.source_degree = source_degree
+        self.source_reliability_range = source_reliability_range
 
     def run(self):
         print(f"Started simulation at {time.ctime()}")
@@ -81,129 +77,6 @@ class Simulation:
             f"folder, {self.folder_communities}\n"
             f"number_of_communities, {self.number_of_communities}\n"
             f"number_of_voting_simulations, {self.number_of_voting_simulations}\n"
-            f"number_of_nodes, {self.number_of_agents}\n"
-            f"degree, {self.degree}\n"
-            f"probability_preferential_attachment"
-            f", {self.probability_preferential_attachment}\n"
-            f"elite_competence_range, {self.elite_competence_range}\n"
-            f"mass_competence_range, {self.mass_competence_range}\n"
-            f"number_of_elites_range, {self.number_of_elites_range}\n"
-            f"probability_homophilic_attachment_range, "
-            f"{self.probability_homophilic_attachment_range}"
-        )
-        filename_readme = f"{self.folder_communities}/README.csv"
-        with open(filename_readme, "w") as f:
-            f.write(information)
-
-    def generate_community(self):
-        probability_homophilic_attachment: float = rd.uniform(
-            *self.probability_homophilic_attachment_range
-        )
-        number_of_elites: int = rd.randint(*self.number_of_elites_range)
-
-        # 1. Generate community with these parameters
-        community = Community(
-            number_of_agents=self.number_of_agents,
-            number_of_elites=number_of_elites,
-            influence_degree=self.degree,
-            probability_preferential_attachment=(
-                self.probability_preferential_attachment
-            ),
-            probability_homophilic_attachment=probability_homophilic_attachment,
-        )
-        return community
-
-    def write_head_line(self):
-        head_line = (
-            "community_number,"
-            + "collective_accuracy,"
-            + "collective_accuracy_precision,"
-            + "minority_competence,"
-            + "majority_competence,"
-            + "number_of_minority,"
-            + "influence_minority_proportion,"
-            + "homophily"
-        )
-        with open(self.filename_csv, "w") as f:
-            f.write(head_line)
-
-    def simulate_and_write_data_line(self, community: Community, number: int):
-        # Determine influence_minority_proportion
-        total_influence_minority = community.total_influence_elites()
-        total_influence_majority = community.total_influence_mass()
-        influence_minority_proportion = total_influence_minority / (
-            total_influence_minority + total_influence_majority
-        )
-        # Run voting simulations to estimate accuracy
-        result = community.estimated_community_accuracy(
-            self.number_of_voting_simulations
-        )
-        collective_accuracy = result["accuracy"]
-        collective_accuracy_precision = result["precision"]
-
-        # Print results to line in csv folder_communities
-        data_line = (
-            f"{number},{collective_accuracy},{collective_accuracy_precision},"
-            f"{community.number_of_elites},{influence_minority_proportion},"
-            f"{community.probability_homophilic_attachment}"
-        )
-        with open(self.filename_csv, "a") as f:
-            f.write(f"\n{data_line}")
-
-    def report_progress(self, community_number):
-        stamps_percent = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-        stamps_numbers = [
-            (percent / 100) * self.number_of_communities for percent in stamps_percent
-        ]
-        if community_number in stamps_numbers:
-            progress = int((community_number * 100) / self.number_of_communities)
-            current_time_sec = time.time()
-            elapsed_time_sec = current_time_sec - self.start_time
-            estimated_total_time_sec = (elapsed_time_sec / progress) * 100
-            estimated_finish_time_sec = self.start_time + estimated_total_time_sec
-            estimated_finish_time_clock = time.ctime(estimated_finish_time_sec)
-            print(
-                f"Progress: {progress}%\n"
-                f"Estimated finish time: {estimated_finish_time_clock}"
-            )
-
-
-class SimulationTeams(Simulation):
-    def __init__(
-        self,
-        group_size=10,
-        group_types=("best", "most_diverse", "random"),
-        number_of_sources=10,
-        source_degree=5,
-        source_reliability_range=(0.5, 0.7),
-        *args,
-        **kwargs,
-    ):
-        super(SimulationTeams, self).__init__(*args, **kwargs)
-        self.group_size = group_size
-        self.group_types = group_types
-        self.number_of_sources = number_of_sources
-        self.source_degree = source_degree
-        self.source_reliability_range = source_reliability_range
-
-    def generate_community(self):
-        # 1. Generate community with these parameters
-        community = Community(
-            number_of_agents=self.number_of_agents,
-            number_of_sources=self.number_of_sources,
-            source_degree=self.source_degree,
-            sources_reliability_range=self.source_reliability_range,
-            edges=[],
-        )
-        return community
-
-    def write_readme(self):
-        information = (
-            f"parameter, value\n"
-            f"filename, {self.filename_csv}\n"
-            f"folder, {self.folder_communities}\n"
-            f"number_of_communities, {self.number_of_communities}\n"
-            f"number_of_voting_simulations, {self.number_of_voting_simulations}\n"
             f"number_of_agents, {self.number_of_agents}\n"
             f"group_size, {self.group_size}\n"
             f"number_of_sources, {self.number_of_sources}\n"
@@ -213,6 +86,18 @@ class SimulationTeams(Simulation):
         filename_readme = f"{self.folder_communities}/README.csv"
         with open(filename_readme, "w") as f:
             f.write(information)
+
+    def generate_community(self):
+        # 1. Generate community with these parameters
+        community = Community(
+            number_of_agents=self.number_of_agents,
+            number_of_sources=self.number_of_sources,
+            influence_degree=self.influence_degree,
+            source_degree=self.source_degree,
+            sources_reliability_range=self.source_reliability_range,
+            edges=[],
+        )
+        return community
 
     def write_head_line(self):
         head_line = (
@@ -250,34 +135,26 @@ class SimulationTeams(Simulation):
             group = []
 
             if group_type == "best":
-                best_result = best_group(
-                    community=community, group_size=self.group_size
-                )
+                best_result = best_team(community=community, group_size=self.group_size)
                 group = best_result["group"]
                 community_new.set_source_network(best_result["source_network"])
             if group_type == "most_diverse":
-                diverse_result = most_diverse_group(
+                diverse_result = most_diverse_team(
                     community=community, group_size=self.group_size
                 )
                 group = diverse_result["group"]
                 community_new.set_source_network(diverse_result["source_network"])
             if group_type == "random":
-                random_result = random_group(
+                random_result = random_team(
                     community=community, group_size=self.group_size
                 )
                 group = random_result["group"]
                 community_new.set_source_network(random_result["source_network"])
-            # if group_type == "optimal":
-            #     group = community.best_group(group_size=self.group_size)
-            # if group_type == "diverse":
-            #     group = community.diverse_group(
-            #         group_size=self.group_size
-            #     )
 
             group_params["group"] = group
             group_params["community"] = community_new
 
-            result = group_simulations(**group_params)
+            result = team_simulation(**group_params)
             accuracy: float = result["accuracy"]
             precision: float = result["precision"]
             diversity: float = result["diversity"]
@@ -287,14 +164,31 @@ class SimulationTeams(Simulation):
         with open(self.filename_csv, "a") as f:
             f.write(f"\n{data_line}")
 
+    def report_progress(self, community_number):
+        stamps_percent = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+        stamps_numbers = [
+            (percent / 100) * self.number_of_communities for percent in stamps_percent
+        ]
+        if community_number in stamps_numbers:
+            progress = int((community_number * 100) / self.number_of_communities)
+            current_time_sec = time.time()
+            elapsed_time_sec = current_time_sec - self.start_time
+            estimated_total_time_sec = (elapsed_time_sec / progress) * 100
+            estimated_finish_time_sec = self.start_time + estimated_total_time_sec
+            estimated_finish_time_clock = time.ctime(estimated_finish_time_sec)
+            print(
+                f"Progress: {progress}%\n"
+                f"Estimated finish time: {estimated_finish_time_clock}"
+            )
 
-def group_simulations(
+
+def team_simulation(
     community: Community,
     group: list,
     number_of_voting_simulations: int = 10,
     alpha: float = 0.05,
 ):
-    """ Method for estimating the group accuracy of a group of agents in a community.
+    """ Method for estimating the team accuracy of a group of agents in a community.
     :param community: Community
         Community
     :param group
