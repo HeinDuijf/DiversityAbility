67259b2323974cc840e115ee347626b0920da53a
HeinDuijf <duijf.hein@gmail.com>
2024-07-04 10:35:52 +0200

    revised using dataframe

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+import pandas as pd
+
 from simulation import Simulation
 
 if __name__ == "__main__":
-    params = {
-        "team_types": ["best", "most_diverse", "random"],
-        "source_reliability_range": (0.5, 0.7),
-        "number_of_agents": 20,
-        "number_of_communities": 10 ** 3,
-        "number_of_voting_simulations": 10 ** 3,
-        "source_degree": 5,
-    }
-    group_sizes = [5, 11]  # 21]
-    sources_range = [11, 21]  # 21, 31]
-    for number_of_sources in sources_range:
-        for group_size in group_sizes:
-            print(
-                f"Started: Number of sources {number_of_sources} | Group size:"
-                f" {group_size}"
-            )
-            Simulation(
-                group_size=group_size, number_of_sources=number_of_sources, **params,
-            ).run()
+    columns = [
+        "team_types",
+        "number_of_agents",
+        "number_of_communities",
+        "number_of_voting_simulations",
+        "source_degree",
+        "sources_reliability_distribution",
+        "group_size",
+        "number_of_sources",
+    ]
+    team_types = ["best", "most_divers", "random"]
+    data = [
+        [team_types, 20, 10**4, 10**4, 5, rel_range, group_size, n_sources]
+        for n_sources in [11, 21, 31]
+        for group_size in [5, 11, 21]
+        for rel_range in [(0.45, 0.65), (0.5, 0.7), (0.55, 0.75), (0.6, 0.8)]
+    ]
+    df = pd.DataFrame(data=data, columns=columns)
+    print(df)
+    for row in df.iterrows():
+        print(f"Simulation {row[0]}")
+        params = row[1]
+        Simulation(**params).run()
