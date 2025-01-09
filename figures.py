c2ec0e694ab4ca2f482a201d69eb1de59427548d
HeinDuijf <duijf.hein@gmail.com>
2025-01-09 10:29:49 +0100

    added some figures

--- figures.py
+++ figures.py
@@ -3,13 +3,13 @@ from figures_scripts.hongpage import figure_hong_page
 from figures_scripts.individual_scores import boxplot_individual_scores
 
 if __name__ == "__main__":
-    # visualize_heatmap(outcome="accuracy")
-    # visualize_heatmap(outcome="pool_accuracy")
-    # visualize_heatmap(outcome="bounded_pool_accuracy")
-    # visualize_heatmap(outcome="accuracy", measure="relative")
-    # visualize_heatmap(outcome="pool_accuracy", measure="relative")
-    # visualize_heatmap(outcome="bounded_pool_accuracy", measure="relative")
-    # visualize_heatmap(outcome="average", measure="absolute")
-    # visualize_heatmap(outcome="average", measure="relative")
+    visualize_heatmap(outcome="accuracy")
+    visualize_heatmap(outcome="pool_accuracy")
+    visualize_heatmap(outcome="bounded_pool_accuracy")
+    visualize_heatmap(outcome="accuracy", measure="relative")
+    visualize_heatmap(outcome="pool_accuracy", measure="relative")
+    visualize_heatmap(outcome="bounded_pool_accuracy", measure="relative")
+    visualize_heatmap(outcome="average", measure="absolute")
+    visualize_heatmap(outcome="average", measure="relative")
     boxplot_individual_scores()
-    # figure_hong_page()
+    figure_hong_page()
