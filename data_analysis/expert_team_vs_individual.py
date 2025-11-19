e85fdb06a2f3febbb42f7036640ad50aed13e251
HeinDuijf <duijf.hein@gmail.com>
2025-11-19 17:03:09 +0100

    simplified

--- data_analysis/expert_team_vs_individual.py
+++ data_analysis/expert_team_vs_individual.py
@@ -1,56 +1,53 @@
-import os
-
 import pandas as pd
 
 from models.generate_teams import generate_expert_team
 from models.sources import Sources
 
 
-def produce_df_expert_team_individual(outcome: str = "accuracy", n_decimals=3):
-    date = "202412"
-    files = [
-        file
-        for file in os.listdir("data")
-        if file[:10] == "simulation" and date in file and "README" not in file
-    ]
-    results = []
-    for file in files:
-        df = pd.read_csv(f"data/{file}")
-        n_sources = df.at[0, "n_sources"]
-        rel_mean = df.at[0, "reliability_mean"]
-        heuristic_size = df.at[0, "heuristic_size"]
+def produce_df_expert_team_individual(
+    outcome: str = "accuracy_opinion", n_decimals=3
+) -> pd.DataFrame:
+    results: list = []
+    for n_sources in [13, 17]:
+        for rel_mean in [0.55, 0.6, 0.65, 0.7, 0.75]:
+            heuristic_size = 5
+            sources = Sources(n_sources, ("equi", rel_mean, 0.2))
+            team_dummy = generate_expert_team(sources, heuristic_size, 1)
+            best_individual = team_dummy.members[0]
+            best_individual_score = best_individual.score
 
-        rel_range = (rel_mean - 0.1, rel_mean + 0.1)
-        sources = Sources(n_sources, ("equi", rel_range))
-        team_dummy = generate_expert_team(sources, heuristic_size, 1)
-        best_individual = team_dummy.members[0]
+            expert_team = generate_expert_team(sources, heuristic_size, team_size=9)
+            expert_team_outcome = 0.0
+            if outcome == "accuracy_opinion":
+                expert_team_outcome, _ = expert_team.accuracy_opinion()
+            elif outcome == "accuracy_evidence":
+                expert_team_outcome = expert_team.accuracy_evidence()
+            elif outcome == "accuracy_bounded":
+                expert_team_outcome, _ = expert_team.accuracy_bounded()
 
-        expert_team_outcome = df.at[0, outcome]
-        best_individual_score = best_individual.score
+            individual_error = 1 - best_individual_score
+            expert_team_error = 1 - expert_team_outcome
 
-        individual_error = 1 - best_individual_score
-        expert_team_error = 1 - expert_team_outcome
+            if expert_team_error > individual_error:
+                error_reduction = (
+                    100 * (expert_team_error - individual_error) / individual_error
+                )
+                # error_reduction = - (expert_error / diverse_error)
+            elif individual_error > expert_team_error:
+                error_reduction = (
+                    -100 * (individual_error - expert_team_error) / expert_team_error
+                )
+                # error_reduction = diverse_error / expert_error
+            else:
+                error_reduction = 0
 
-        if expert_team_error > individual_error:
-            error_reduction = (
-                100 * (expert_team_error - individual_error) / individual_error
+            results.append(
+                [
+                    n_sources,
+                    rel_mean,
+                    round(best_individual_score - expert_team_outcome, n_decimals),
+                    round(error_reduction),
+                ]
             )
-            # error_reduction = - (expert_error / diverse_error)
-        elif individual_error > expert_team_error:
-            error_reduction = (
-                -100 * (individual_error - expert_team_error) / expert_team_error
-            )
-            # error_reduction = diverse_error / expert_error
-        else:
-            error_reduction = 0
-
-        results.append(
-            [
-                n_sources,
-                rel_mean,
-                round(best_individual_score - expert_team_outcome, n_decimals),
-                round(error_reduction),
-            ]
-        )
     columns = ["n_sources", "rel_mean", "difference", "error_reduction"]
     return pd.DataFrame(results, columns=columns)
--- figures_scripts/individual_scores.py
+++ figures_scripts/individual_scores.py
@@ -1,4 +1,3 @@
-import os
 from math import comb
 
 import matplotlib.pyplot as plt
@@ -10,38 +9,31 @@ from models.generate_teams import generate_expert_team
 from models.sources import Sources
 
 
-def boxplot_individual_scores(date="202412"):
-    files = [
-        file
-        for file in os.listdir("data")
-        if file[:10] == "simulation" and date in file and "README" not in file
-    ]
-    scores_df_13 = pd.DataFrame()
-    for file in files:
-        df = pd.read_csv(f"data/{file}")
-        rel_mean = df.at[0, "reliability_mean"]
-        n_sources = df.at[0, "n_sources"]
-        if n_sources != 13:
-            break
-        heuristic_size = df.at[0, "heuristic_size"]
-        reliability_distribution = ("equi", (rel_mean - 0.1, rel_mean + 0.1))
+def boxplot_individual_scores(n_sources: int = 13, heuristic_size: int = 5, show=False):
+    scores_df = pd.DataFrame()
+    rel_mean_list = [0.55, 0.6, 0.65, 0.7, 0.75]
+    for rel_mean in rel_mean_list:
+        reliability_distribution = ("equi", rel_mean, 0.2)
         sources = Sources(n_sources, reliability_distribution)
         n_possible_heuristics = comb(n_sources, heuristic_size)
         team = generate_expert_team(sources, heuristic_size, n_possible_heuristics)
         data = np.array([agent.score for agent in team.members])
         data_df = pd.DataFrame(data, columns=[rel_mean])
-        scores_df_13 = pd.concat([scores_df_13, data_df], axis=1)
+        scores_df = pd.concat([scores_df, data_df], axis=1)
 
     sns.set_style("whitegrid")
     font_style = {"family": "Times New Roman", "size": 12}
     plt.rc("font", **font_style)
     plt.figure(figsize=(6, 3))
 
-    fig = sns.boxplot(data=scores_df_13, palette="Grays")
+    fig = sns.boxplot(data=scores_df, palette="Grays")
     fig.set_yticks(0.4 + 0.1 * np.arange(7, dtype=int))
     # fig.set_title("Individual scores")
     fig.set_xlabel("Mean source reliability")
     fig.set_ylabel("Score")
+    if show:
+        plt.show()
+
     plt.savefig(
         "figures/individual_scores.eps", bbox_inches="tight", dpi=800, format="eps"
     )
@@ -49,33 +41,28 @@ def boxplot_individual_scores(date="202412"):
     plt.close()
 
 
-def df_individual_scores(date="202412"):
-    files = [
-        file
-        for file in os.listdir("data")
-        if file[:10] == "simulation" and date in file and "README" not in file
-    ]
+def df_individual_scores(n_sources: int = 13, heuristic_size: int = 5):
     data_scores = np.array([])
-    for file in files:
-        df = pd.read_csv(f"data/{file}")
-        rel_mean = df.at[0, "reliability_mean"]
-        n_sources = df.at[0, "n_sources"]
-        heuristic_size = df.at[0, "heuristic_size"]
-        reliability_distribution = ("equi", (rel_mean - 0.1, rel_mean + 0.1))
-        sources = Sources(n_sources, reliability_distribution)
-        n_possible_heuristics = comb(n_sources, heuristic_size)
-        team = generate_expert_team(sources, heuristic_size, n_possible_heuristics)
-        data = np.array([agent.score for agent in team.members])
-        data_mean = data.mean()
-        data_std = data.std()
-        data_max = data.max()
-        data_min = data.min()
-        data_scores = np.concatenate(
-            [
-                data_scores,
-                [n_sources, rel_mean, data_mean, data_std, data_max, data_min],
-            ]
-        )
+    scores_df = pd.DataFrame()
+    rel_mean_list = [0.55, 0.6, 0.65, 0.7, 0.75]
+    for n_sources in [13, 17]:
+        for rel_mean in rel_mean_list:
+            reliability_distribution = ("equi", rel_mean, 0.2)
+            sources = Sources(n_sources, reliability_distribution)
+            n_possible_heuristics = comb(n_sources, heuristic_size)
+            team = generate_expert_team(sources, heuristic_size, n_possible_heuristics)
+            data = np.array([agent.score for agent in team.members])
+            data = np.array([agent.score for agent in team.members])
+            data_mean = data.mean()
+            data_std = data.std()
+            data_max = data.max()
+            data_min = data.min()
+            data_scores = np.concatenate(
+                [
+                    data_scores,
+                    [n_sources, rel_mean, data_mean, data_std, data_max, data_min],
+                ]
+            )
 
     data_scores = np.reshape(data_scores, (10, 6))
     scores_df = pd.DataFrame(
