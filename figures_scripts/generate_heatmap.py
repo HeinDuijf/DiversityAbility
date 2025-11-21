94d8b83322604eb01a31839cbdda4537b987d973
HeinDuijf <duijf.hein@gmail.com>
2025-11-21 22:39:53 +0100

    cleaned up, added n_sources_list and show_cbar args, added comma for effect size

--- figures_scripts/generate_heatmap.py
+++ figures_scripts/generate_heatmap.py
@@ -7,20 +7,21 @@ from data_analysis.statistics import produce_df_1samp
 def heatmap(
     outcome: str = "accuracy",
     diverse_team_type: str = "diverse",
-    heuristic_size: int = 5,
+    heuristic_size: int | list[int] = 5,
+    n_sources_list: list[int] = [13, 17],
     measure: str = "absolute",
-    mask: bool = True,
     colors: bool = False,
     show: bool = False,
+    show_cbar: bool = True,
     filename: str | None = None,
 ):
     df = produce_df_1samp(
         outcome=outcome,
         diverse_team_type=diverse_team_type,
         heuristic_size=heuristic_size,
+        n_sources_list=n_sources_list,
         perform_bca_ci=False,
     )
-    df = df[df["n_sources"] < 21]
     if measure == "absolute":
         df["effect_percent"] = 100 * df["difference"]
     if measure == "relative":
@@ -35,64 +36,65 @@ def heatmap(
     sns.set_style("white")
     font_style = {"family": "Times New Roman", "size": 12}
     plt.rc("font", **font_style)
-    plt.figure(figsize=(2, 3))
+    plt.figure(figsize=(5, 3))
 
     heatmap_params = {
         # "annot": True,
         "cmap": "gray_r",  # "coolwarm"
         "square": True,
+        "cbar": show_cbar,
         "cbar_kws": {"shrink": 0.4},
+        "vmin": 0,
+        "vmax": 10,
+        "fmt": "",
     }
     df_heatmap = abs(pivot_df)
+    annot_df = pivot_df.copy().map(lambda x: f"{x:.1f}")
+
     if colors:
         heatmap_params["cmap"] = "coolwarm"
         heatmap_params["center"] = 0
         df_heatmap = pivot_df
 
     if measure == "absolute":
-        heatmap_params["vmin"] = -10
-        heatmap_params["vmax"] = 10
-        heatmap_params["annot"] = True
-        if not colors:
-            labels = pivot_df.copy()
-            labels = labels.map(
-                lambda x: (f"+{x:.1f}" if "-" not in str(x) else f"{x:.1f}")
-            )
-            heatmap_params["annot"] = labels
-            heatmap_params["fmt"] = ""
-            heatmap_params["vmin"] = 0
+        if colors:
+            heatmap_params["vmin"] = -10
+            heatmap_params["vmax"] = 10
+        else:
+            positives_df = pivot_df > 0.0
+            annot_df[positives_df] = "+" + annot_df[positives_df]
 
     if measure == "relative":
-        heatmap_params["vmin"] = -100
+        annot_df = pivot_df.copy().map(lambda x: f"{x:.0f}")
         heatmap_params["vmax"] = 100
-        heatmap_params["fmt"] = "g"
-        heatmap_params["annot"] = True
-        if not colors:
-            labels = pivot_df.copy()
-            labels = labels.map(
-                lambda x: f"+{x:.0f}" if "-" not in str(x) else f"{x:.0f}"
-            )
-            heatmap_params["annot"] = labels
-            heatmap_params["fmt"] = ""
-            heatmap_params["vmin"] = 0
 
-    if not mask:
-        fig = sns.heatmap(df_heatmap, **heatmap_params)
-    else:
-        mask_df = df.pivot(
-            index="rel_mean",
-            columns="n_sources",
-            values="p_value",
-        )
-        mask_df.fillna(value=0.0, inplace=True)
-        mask_df = mask_df.astype(bool)
-        mask_df.sort_index(inplace=True, ascending=False)
-        fig = sns.heatmap(
-            df_heatmap,
-            mask=mask_df,
-            # annot=labels,
-            **heatmap_params,
-        )
+        if colors:
+            heatmap_params["vmin"] = -100
+            heatmap_params["vmax"] = 100
+        else:
+            positives_df = pivot_df > 0.0
+            annot_df[positives_df] = "+" + annot_df[positives_df]
+
+    effect_df = df.pivot(index="rel_mean", columns="n_sources", values="effect_size")
+    effect_low = effect_df < 0.1
+    effect_mid = (effect_df >= 0.1) & (effect_df < 0.3)
+    annot_df[effect_low] = annot_df[effect_low] + "'"
+    annot_df[effect_mid] = annot_df[effect_mid] + "''"
+
+    pvalue_df = df.pivot(
+        index="rel_mean",
+        columns="n_sources",
+        values="p_value",
+    )
+    not_sig = pvalue_df > 0.001
+    annot_df[not_sig] = ""
+
+    heatmap_params["annot"] = annot_df
+
+    fig = sns.heatmap(
+        df_heatmap,
+        **heatmap_params,
+    )
 
     fig.set_xlabel("Sources (#)")
     fig.set_ylabel("Reliability (mean)")
