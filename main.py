79b315a1dd35e16325a206798f8d389f3806120e
HeinDuijf <duijf.hein@gmail.com>
2025-12-05 17:27:20 +0100

    reset to main simulations

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from grid_simulation import GridSimulation
 
 if __name__ == "__main__":
-    rels = [
-        ("equi", rel_range)
-        for rel_range in [
-            (0.45, 0.65),
-            (0.5, 0.7),
-            # (0.55, 0.75),
-            # (0.6, 0.8),
-            # (0.65, 0.85),
-        ]
-    ]
+    rels = [("equi", rel_mean, 0.2) for rel_mean in [0.55, 0.6, 0.65, 0.7, 0.75]]
+
     GridSimulation(
-        team_types=["qualified_diverse_90", "diverse"],
-        n_sources_list=[13],
+        team_types=["expert", "diverse"],
+        n_sources_list=[13, 17],
         reliability_distribution_list=rels,
-        n_samples=10**2,
-        estimate_sample_size=10**2,
+        n_samples=10**4,
     ).run()
